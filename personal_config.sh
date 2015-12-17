e9c2fb6bcb770d91ad82b9a43f1b70950e1921d4
unknown <hvu@NCEL28968.iis.amadeus.net>
2015-12-17 16:41:54 +0100

    modify: personal_config.sh : improve by verifying if emacs installed, if not install it

--- personal_config.sh
+++ personal_config.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #File personal_config.sh : PUT THIS FILE INTO A SHELL PROVISION 
 
-printf "\e[1;33m%s\n\e[0m" "START executing personal_config.sh file......." 
+printf "\e[1;33m%s\e[0m\n" "START executing personal_config.sh file......." 
 
 HOME_USER=/home/vagrant
 CONFIG_FOLDER=${HOME_USER}/config
@@ -12,6 +12,12 @@ EMACS_FOLDER_DEST=${HOME_USER}/.emacs.d
 EMACS_FILE_SOURCE=${CONFIG_FOLDER}/.emacs
 EMACS_FILE_DEST=${HOME_USER}/.emacs
 
+if command -v emacs > /dev/null; then
+	echo "Already have emacs installed and added to PATH"
+else
+	echo "No emacs found in PATH, installing it now...."
+	apt-get -y install emacs
+fi
 
 if [ ! -d ${CONFIG_FOLDER} ]; then
     sudo -u vagrant mkdir -p ${CONFIG_FOLDER}
@@ -21,12 +27,16 @@ git clone https://github.com/vuhonganh/personal_config.git ${CONFIG_FOLDER}
 
 if [ ! -d ${EMACS_FOLDER_DEST} ]; then	
 	sudo -u vagrant cp -r ${EMACS_FOLDER_SOURCE} ${EMACS_FOLDER_DEST}
+else	
+	echo "Already have a .emacs.d folder, we let you do configuration manually on your own"
 fi
 
 if [ ! -e ${EMACS_FILE_DEST} ]; then
 	sudo -u vagrant cp ${EMACS_FILE_SOURCE} ${EMACS_FILE_DEST}
+else	
+	echo "Already have a .emacs configuration file, we let you do configuration manually on your own"
 fi
 
-printf "\n\e[1;33m%s\n\e[0m" "DONE executing personal_config.sh file......."
+printf "\n\e[1;33m%s\e[0m\n" "DONE executing personal_config.sh file......."
 
 
